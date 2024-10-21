8e70eb3a6290ad6af5093ccc65d8bb1caa8b1eeb
sumborwonpob <sumethreeprasit.borwonpob@terra-drone.co.jp>
2024-10-21 15:53:18 +0900

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# VL53L5CX_I2C_DRIVER
-An implementation of VL53L5CX I2C driver based on STM's VL53L5CX Ultra-light Driver (ULD) 2.0.0. 
+# VL53L8CX_I2C_DRIVER
+An implementation of VL53L8CX I2C driver based on STM's VL53L8CX Ultra-light Driver (ULD) 2.0.0.
+https://www.st.com/en/embedded-software/stsw-img040.html
+
+## Environment tested
+Device: Pololu VL53L8CX carrier board
+https://www.pololu.com/product/3419
+
+Host Device: Raspberry Pi 4B / Orange Pi 5
+
+OS: Ubuntu 22:04
+
+Protocol: I2C
+
+## Pre-requisite
+* Enable RPi's I2C and install libi2c-dev
+```
+sudo apt install raspi-config # Install config program
+sudo raspi-config # Open config
+
+# Select 3 Interface Options > 15 I2C
+# Enable it
+
+sudo apt-get install libi2c-dev # Install C library for i2c
+
+i2cdetect -y 1 # Replace the 1 with the I2C bus you are using.
+# Note that the address shown in this command may not be the actual address.
+# According to the Datasheet, the sensor's default address is 0x52.
+```
+* For some devices, there might be a failure to opening /dev/i2c-*. You may need to add the user to the i2c group.
+```
+sudo adduser <your_username> i2c #Put your device's username
+sudo reboot
+```
+
+
+## Connections
+[Carrier Board] -> [RPi]
+
+Vcc -> 3.3V
+
+GND -> GND
+
+SCL -> SCL
+
+SDA -> SDA
+
+SPI/I2C -> GND
+
+## In code usage
+Refer to the example.cpp for the basic usage.
+
+Additional usage can be found in the original ULD.
---
+++ example.cpp
@@ -0,0 +1,116 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "VL53L5CX_I2C_DRIVER/vl53l5cx_api.h"
+#include "VL53L5CX_I2C_DRIVER/vl53l5cx_api.c"
+#include "VL53L5CX_I2C_DRIVER/platform.h"
+#include "VL53L5CX_I2C_DRIVER/platform.c"
+
+// :::::::::::::::::::::::::::::::::: MAIN :::::::::::::::::::::::::::::::::::::::
+int main(int argc, char * argv[])
+{
+	/*********************************/
+	/*   VL53L5CX ranging variables  */
+	/*********************************/
+
+	uint8_t 				status, loop, isAlive, isReady, i;
+	VL53L5CX_Configuration 	Dev;			/* Sensor configuration */
+	VL53L5CX_ResultsData 	Results;		/* Results data from VL53L5CX */
+
+
+	/*********************************/
+	/*      Customer platform        */
+	/*********************************/
+
+	/* Fill the platform structure with customer's implementation. For this
+	* example, only the I2C address is used.
+	*/
+	Dev.platform.address = 0x52;
+	auto p_dev = &Dev.platform;
+
+    /* Initialize sensor's file descriptor */
+	int init_ret = VL53L5CX_Comms_Init(p_dev, "/dev/i2c-5");
+	if(init_ret != 0) exit(init_ret);
+
+	/* (Optional) Reset sensor toggling PINs (see platform, not in API) */
+	//VL53L5CX_Reset_Sensor(&(Dev.platform));
+
+	/* (Optional) Set a new I2C address if the wanted address is different
+	* from the default one (filled with 0x20 for this example).
+	*/
+	//status = vl53l5cx_set_i2c_address(&Dev, 0x20);
+
+
+	/*********************************/
+	/*   Power on sensor and init    */
+	/*********************************/
+
+	/* (Optional) Check if there is a VL53L5CX sensor connected */
+	status = vl53l5cx_is_alive(&Dev, &isAlive);
+	if(!isAlive || status)
+	{
+		printf("VL53L5CX not detected at requested address\n");
+		return status;
+	}
+	else
+	{
+		printf("VL53L5CX is there\n");
+	}
+
+	/* (Mandatory) Init VL53L5CX sensor */
+	status = vl53l5cx_init(&Dev);
+	if(status)
+	{
+		printf("VL53L5CX ULD Loading failed\n");
+		return status;
+	}
+
+	printf("VL53L5CX ULD ready ! (Version : %s)\n",
+			VL53L5CX_API_REVISION);
+
+	/*********************************/
+	/*         Ranging loop          */
+	/*********************************/
+
+	status = vl53l5cx_start_ranging(&Dev);
+
+	loop = 0;
+	while(loop < 10)
+	{
+		/* Use polling function to know when a new measurement is ready.
+		 * Another way can be to wait for HW interrupt raised on PIN A1
+		 * (INT) when a new measurement is ready */
+ 
+		status = vl53l5cx_check_data_ready(&Dev, &isReady);
+
+		if(isReady)
+		{
+			vl53l5cx_get_ranging_data(&Dev, &Results);
+
+			/* As the sensor is set in 4x4 mode by default, we have a total 
+			 * of 16 zones to print. For this example, only the data of first zone are 
+			 * print */
+			printf("Print data no : %3u\n", Dev.streamcount);
+			for(i = 0; i < 16; i++)
+			{
+				printf("Zone : %3d, Status : %3u, Distance : %4d mm\n",
+					i,
+					Results.target_status[VL53L5CX_NB_TARGET_PER_ZONE*i],
+					Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE*i]);
+			}
+			printf("\n");
+			loop++;
+		}
+
+		/* Wait a few ms to avoid too high polling (function in platform
+		 * file, not in API) */
+		VL53L5CX_WaitMs(&(Dev.platform), 5);
+	}
+
+	status = vl53l5cx_stop_ranging(&Dev);
+
+	/* Close file descriptor */
+	VL53L5CX_Comms_Close(p_dev);
+}
